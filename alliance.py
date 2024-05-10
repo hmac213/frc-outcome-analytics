22bd3ba6a78d513ed546cb356bdb2146ab99373c
Henry McNamara <hmac213@gmail.com>
2024-05-10 12:16:09 -0700

    working to fix match sorting system

--- alliance.py
+++ alliance.py
@@ -1,27 +1,51 @@
-import statbotics
 import requests
 import string
 
 auth_TBA = {'X-TBA-Auth-Key' : '319O7fm4AYptga0ktUY6oIW4uLfUqatprLsxyyFkymObLkYbo7u4lSi8jJ9UJT3f'}
 
-sb = statbotics.Statbotics()
-
 class alliance:
     def __init__(self, event_code, seed, bracket_style):
+        # fetch alliance data from API and convert from json
         get_alliances_json = requests.get(f'https://www.thebluealliance.com/api/v3/event/{event_code}/alliances', params = auth_TBA)
         get_alliances = get_alliances_json.json()
+
+        # initializing alliance characteristics
         self.event_code = event_code
         self.seed_num = seed
-        self.seed_name = "Alliance " + str(seed)
+        self.bracket_style = bracket_style
         self.team1 = get_alliances[self.seed_num - 1]['picks'][0]
         self.team2 = get_alliances[self.seed_num - 1]['picks'][1]
         self.team3 = get_alliances[self.seed_num - 1]['picks'][2]
+        self.matches = []
+        self.match_colors = []
+        
+        # only include fourth team if it exists
         if len(get_alliances[self.seed_num - 1]['picks']) == 4:
             self.team4 = get_alliances[self.seed_num - 1]['picks'][3]
 
-        self.matches = []
-        self.bracket_style = bracket_style
-
-    def assign_match(self, matches):
-        for match in matches:
+    def init_match(self, match, color):
             self.matches.append(match)
+            self.match_colors.append(color)
+
+    # sorting an alliance's matches in chronological order
+    def sort_matches(self):
+        sorted_matches = []
+        if self.bracket_style == 'old':
+            for match in self.matches:
+                if match['comp-level'] == 'qf':
+                    sorted_matches.append(match)
+                elif match['comp-level'] == 'sf':
+                    sorted_matches.append(match)
+                else:
+                    sorted_matches.append(match)
+        elif self.bracket_style == 'new':
+            for match_index in range(len(self.matches)):
+                for index in range(len(self.matches)):
+                    if '_sf' + str(match_index + 1) + 'm' in self.matches[index]['key']:
+                        sorted_matches.append(self.matches[match_index])
+                    if '_f' in self.matches[index]['key'] and 'm' + str(match_index + 1) in self.matches[match_index]['key']:
+                        sorted_matches.append(self.matches[match_index])
+        
+        self.matches = sorted_matches
+        for match in self.matches:
+            print('match sorted for alliance ' + str(self.seed_num) + ': ' + match['key'])
--- event.py
+++ event.py
@@ -1,4 +1,3 @@
-import statbotics
 import requests
 import string
 from alliance import alliance
@@ -7,30 +6,48 @@ auth_TBA = {'X-TBA-Auth-Key' : '319O7fm4AYptga0ktUY6oIW4uLfUqatprLsxyyFkymObLkYb
 
 class event:
     def __init__(self, event_code):
+        # get event data from API and convert from json
         get_event_matches_json = requests.get(f'https://www.thebluealliance.com/api/v3/event/{event_code}/matches', params = auth_TBA)
         get_event_matches = get_event_matches_json.json()
 
+        # initializing event characteristics
         self.event_code = event_code
-
         self.alliances = []
+        self.match_list = []
+
+        # initializing alliances and bracket type
         for alliance_num in range(8):
             if int(self.event_code.strip(string.ascii_letters)) < 2023:
-                self.alliances.append(alliance(event_code, alliance_num, 'old'))
+                self.alliances.append(alliance(event_code, alliance_num + 1, 'old'))
             else:
-                self.alliances.append(alliance(event_code, alliance_num, 'new'))
-
-        self.match_list = []
-        def init_matches():
+                self.alliances.append(alliance(event_code, alliance_num + 1, 'new'))
+        
+        # initializing matches for all alliances
+        def init_matches(self):
             for match in get_event_matches:
-                if match['comp_level'] != 'qm':
+                if match['comp_level'] != 'qm' and match['actual_time'] != None:
                     self.match_list.append(match)
             for match in self.match_list:
                 for alliance in self.alliances:
-                    if alliance.team1 in match['alliances']['team_keys'] or alliance.team1 in match['alliances']['surrogate_team_keys']:
-                        alliance.init_match(match)
+                    if alliance.team1 in match['alliances']['red']['team_keys'] or alliance.team1 in match['alliances']['red']['surrogate_team_keys']:
+                        alliance.init_match(match, 'red')
+                    elif alliance.team1 in match['alliances']['blue']['team_keys'] or alliance.team1 in match['alliances']['blue']['surrogate_team_keys']:
+                        alliance.init_match(match, 'blue')
+            for alliance in self.alliances:
+                    alliance.sort_matches()
         
-        init_matches()
+        init_matches(self)
+
+        # define function to calculate final event rankings.
+        self.rankings = [1, 2, 3, 4, 5, 6, 7, 8]
+        def calculate_rankings(self):
+            for alliance in self.alliances:
+                for compare_alliance in self.alliances:
+                    if alliance == compare_alliance:
+                        continue
 
 
+
+cada = event('2024cada')
         
         
