a3afc6c68c5a61200a0f0f19955ec960dcdf6699
Henry McNamara <hmac213@gmail.com>
2024-05-13 10:13:30 -0700

    changed distribution to simulation model for improved accuracy

--- probability.py
+++ probability.py
@@ -1,6 +1,7 @@
 from alliance import alliance
 import statbotics
 import scipy.stats as sp
+import numpy as np
 
 sb = statbotics.Statbotics()
 
@@ -16,7 +17,19 @@ def calculate_win_prob (red_alliance: alliance, blue_alliance: alliance):
 
     # return sp.ttest_ind_from_stats(red_average, red_standard_deviation, num_qual_matches, blue_average, blue_standard_deviation, num_qual_matches, False, 'greater')
 
-    return [red_average, red_standard_deviation, blue_average, blue_standard_deviation, sp.ttest_ind_from_stats(red_average, red_standard_deviation, num_qual_matches, blue_average, blue_standard_deviation, num_qual_matches, False, 'greater')]
+    red_wins = 0
+    blue_wins = 0
 
-print(calculate_win_prob(alliance('2024cada', 1, 'new'), alliance('2024cada', 2, 'new')))
+    for i in range(1000):
+        red_score = red_average + red_standard_deviation * np.random.standard_t(9)
+        blue_score = blue_average + blue_standard_deviation * np.random.standard_t(9)
+        if red_score > blue_score:
+            red_wins += 1
+        else:
+            blue_wins += 1
+
+
+    return [red_average, red_standard_deviation, blue_average, blue_standard_deviation, red_wins / (red_wins + blue_wins), red_wins, blue_wins]
+
+print(calculate_win_prob(alliance('2024cabe', 1, 'new'), alliance('2024cabe', 2, 'new')))
 
--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import statbotics
 import requests
+import numpy as np
 
 auth_TBA = {'X-TBA-Auth-Key' : '319O7fm4AYptga0ktUY6oIW4uLfUqatprLsxyyFkymObLkYbo7u4lSi8jJ9UJT3f'}
 
 sb = statbotics.Statbotics()
 
 match_list = requests.get(f'https://www.thebluealliance.com/api/v3/team/frc254/event/2024cada/matches/simple', params = auth_TBA).json()
+
+event_oprs = requests.get(f'https://www.thebluealliance.com/api/v3/event/2024cada/insights', params = auth_TBA).json()
 
 updated_match_list = []
 for match in match_list:
@@ -17,3 +20,4 @@ match_list = updated_match_list
 print(sb.get_team_event(254, '2024cabe', fields = ['epa_pre_playoffs'])['epa_pre_playoffs'])
 print(sb.get_team_event(1678, '2024cabe', fields = ['epa_pre_playoffs'])['epa_pre_playoffs'])
 print(sb.get_team_event(1160, '2024cabe', fields = ['epa_pre_playoffs'])['epa_pre_playoffs'])
+print(np.random.standard_t(9))
