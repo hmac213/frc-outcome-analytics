153e2acb99cac1cd95f187e8b4d28323f5082e65
hmac213 <hmac213@gmail.com>
2024-06-23 16:07:15 -0700

    updating

--- main.py
+++ main.py
@@ -2,36 +2,21 @@ from event import event
 import statbotics
 import requests
 
-auth_TBA = {'X-TBA-Auth-Key' : '319O7fm4AYptga0ktUY6oIW4uLfUqatprLsxyyFkymObLkYbo7u4lSi8jJ9UJT3f'}
-
 sb = statbotics.Statbotics()
 
-event_list = requests.get(f'https://www.thebluealliance.com/api/v3/events/2024', params = auth_TBA).json()
+event_list = ['2023mxto', '2023nccmp', '2023dal']
 
-event_cumulative_outcomes = {}
+values = []
 
-for entry in event_list:
-    if entry['division_keys'] != [] or entry['event_type_string'] == 'Offseason' or entry['event_type_string'] == 'Preseason':
-        continue
-    if entry['event_code'] == 'flwp':
-        continue
-    this_event = event(str(entry['year']) + entry['event_code'])
-    print(f'starting event: {str(entry['year']) + entry['event_code']}')
+for active_event in event_list:
+    print('starting event: ' + active_event)
+    this_event = event(active_event)
     this_event.create_match_probability_distribution()
-    event_cumulative_outcomes[this_event] = this_event.simulate_brackets()
+    value = this_event.simulate_brackets()
+    values.append(value)
+    print(value)
 
-num_upset_events = 0
-num_typical_events = 0
-for key in event_cumulative_outcomes:
-    if event_cumulative_outcomes[key] < .5:
-        num_upset_events += 1
-    else:
-        num_typical_events += 1
+print(values)
 
-print(num_upset_events, num_typical_events)
-
-# test_event = event('2024flwp')
-# test_event.create_match_probability_distribution()
-# print(test_event.simulate_brackets())
-
+# 8 for 22
 
--- random_select.py
+++ random_select.py
@@ -6,7 +6,7 @@ auth_TBA = {'X-TBA-Auth-Key' : '319O7fm4AYptga0ktUY6oIW4uLfUqatprLsxyyFkymObLkYb
 
 sb = statbotics.Statbotics()
 
-event_list = requests.get(f'https://www.thebluealliance.com/api/v3/events/2024', params = auth_TBA).json()
+event_list = requests.get(f'https://www.thebluealliance.com/api/v3/events/2023', params = auth_TBA).json()
 
 regional_events = []
 district_events = []
@@ -15,7 +15,7 @@ championship_divisions = []
 events = []
 
 for event_item in event_list:
-    if event_item['division_keys'] == []:
+    if event_item['division_keys'] == [] and event_item['event_code'] != 'flwp':
         if event_item['event_type_string'] == 'Regional':
             regional_events.append(event_item['event_code'])
             events.append(event_item['event_code'])
@@ -36,6 +36,9 @@ print(regional_events)
 print(district_events)
 print(district_championships)
 print(championship_divisions)
-print(events[29])
-print(events[30])
-print(events[31])
+
+# We have 175 events: 62 regional, 98 district, 7 dcmps, and 8 championship divisions
+
+# for a percentage breakdown, that is .354, .56, .04, .046
+
+# 160, 15
